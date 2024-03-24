19d912bd12dd420abef2405f24b8753c69e1f125
buchwaldagata <buchwaldagata@gmail.com>
2024-03-24 13:06:22 +0100

    copy class from first task and change it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CoordinateList coordinateList = new CoordinateList("src/resources/kroB100.tsp");
+        int[][] intCoordinateList = coordinateList.intCoordinateList;
+        DistanceMatrix distanceMatrix = new DistanceMatrix(intCoordinateList);
+        Long[][] distanceMatrix2 = distanceMatrix.distanceMatrix;
+
+        GreedyCycleAlgorithm greedyCycleAlgorithm = new GreedyCycleAlgorithm();
+
+        List<Map<Integer, Integer>>  cycles = greedyCycleAlgorithm.runAlgorithm(8, distanceMatrix2);
+        System.out.println(cycles);
+        System.out.println("HEJ");
     }
 }
