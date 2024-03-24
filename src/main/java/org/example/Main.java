337af593c73037c3df2fbd5e61188dfd56aaf609
buchwaldagata <buchwaldagata@gmail.com>
2024-03-24 19:13:47 +0100

    copy class from first task and change it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,43 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        CoordinateList coordinateList = new CoordinateList("src/resources/kroB100.tsp");
+        CoordinateList coordinateList = new CoordinateList("src/resources/kroA100.tsp");
         int[][] intCoordinateList = coordinateList.intCoordinateList;
         DistanceMatrix distanceMatrix = new DistanceMatrix(intCoordinateList);
         Long[][] distanceMatrix2 = distanceMatrix.distanceMatrix;
 
         GreedyCycleAlgorithm greedyCycleAlgorithm = new GreedyCycleAlgorithm();
 
-        List<Map<Integer, Integer>>  cycles = greedyCycleAlgorithm.runAlgorithm(8, distanceMatrix2);
+        List<Map<Integer, Integer>>  cycles = greedyCycleAlgorithm.runAlgorithm(92, distanceMatrix2);
+        Map<Integer, Integer> firstCycle = cycles.get(0);
+        Map<Integer, Integer> secondCycle = cycles.get(1);
         System.out.println(cycles);
+        System.out.println(firstCycle);
+        System.out.println(getLengthFromCycle(firstCycle, distanceMatrix2));
+        System.out.println(getLengthFromCycle(secondCycle,distanceMatrix2));
         System.out.println("HEJ");
+
+
+        MovesAlteringSetsOfVerticesFormingTwoCycles movesAlteringSetsOfVerticesFormingTwoCycles = new MovesAlteringSetsOfVerticesFormingTwoCycles();
+        movesAlteringSetsOfVerticesFormingTwoCycles.run(cycles, distanceMatrix2);
+
+
+    }
+
+
+    public static Long getLengthFromCycle(Map<Integer, Integer> cycle, Long[][] distanceMatrix) {
+        Long len = 0L;
+        for(int i=0; i<cycle.keySet().size(); i++) {
+            int fromVertex = cycle.get(i);
+            int toVertex;
+            if ( i == cycle.keySet().size()-1){
+                toVertex = cycle.get(0);
+            }
+            else{
+                toVertex = cycle.get(i+1);
+            }
+            len += distanceMatrix[fromVertex][toVertex];
+        }
+        return len;
     }
 }
